ae6a9fa2cb59e6834e33d132d5c767358066b770
AbhimanyuKoushik <abhimanyukoushikg@gmail.com>
2025-01-04 12:00:20 +0530

    commit

--- assignments/Problem1/codes/solver.c
+++ assignments/Problem1/codes/solver.c
@@ -1,44 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "functions.h"
 
 //Let the differential equation we want to solve is (an)*(yn)+(an-1)*(yn-1)+...+(a0)*(y)+c=0 where yi is ith derivative of y and ai is a constant
 //The output of the recorddata function will be all the values of yk = y(x+kh) in an array where k is an integer
 
-double* vectorfunction(int order, double coefficients[order+2]){
-	//an = coeff[0], an-1=coeff[1], ... , a1 = coeff[order-1], a0 = coeff[order], c = coeff[order+1]
-	double* outputvector = (double*)malloc(order*sizeof(double));
-	for(int i=1;i<order+1;i++){
-		outputvector[i-1] = -coefficients[i]/coefficients[0];
+double** matrixgen(int order, double coefficients[order+2], double stepsize){
+	//Generates the matrix which must be multiplied to yk to get yk+1
+	double** outputmatrix = identity(order+1);
+	//initializing with identity matrix and changing the elements
+	for(int i=1; i<order; i++){
+		outputmatrix[i][i+1] = stepsize;
 	}
-	return outputvector;
-	//outputs an array with elements [(-(an-1)/an),(-(an-2)/an),...,(-a0/an)]
+	//changing the elements just beside diagonal elements
+	outputmatrix[order][0] = -1/coefficients[0]*stepsize;
+	for(int i=1; i<order+1; i++){
+		outputmatrix[order][i] = (-coefficients[order+1-i]/coefficients[0])*stepsize;
+	}
+	outputmatrix[order][order] += 1; 
+	//changing the last row elements
+	return outputmatrix;
 }
 
 double* recorddata(double lowerbound, double upperbound, int order,  double coefficients[order+2], double initialconditions[order], double stepsize){
-	double y[order];
+	double** vector_y = createMat(order+1,1);
+	vector_y[0][0] = coefficients[order+1];
 	for(int i=0;i<order;i++){
-		y[i] = initialconditions[i];
+		vector_y[i+1][0] = initialconditions[i];
 	}
-	//The vector_y is [y(t), y'(t), ..., yn-1(t)]
+	//The vector_y is [c, y(t), y'(t), ..., yn-1(t)]
 	//It is initialised to the initial conditions which are part of input given by user 
-	double* output_a = vectorfunction(order, coefficients);
-	//output_a is now the array with elements [(-(an-1)/an),(-(an-2)/an),...,(-a0/an)]
+	double** matrix = matrixgen(order, coefficients, stepsize);
+	//the matrix which has to be multiplied is generated
 	int no_datapoints = ((upperbound-lowerbound)/stepsize);
 	//It is max no of points which we want to plot, The lower bound is min value of x, upper bound is max value of x, stepsize is the value of h
 	double* yvalues = malloc(no_datapoints*sizeof(double));
 	for(int i = 0; i<no_datapoints; i++){
 	//generating the vector_y for each value of t = lowerbound+stepsize*i until t reaches upperbound so all the vector_y values for each t are recorded at some point of time
-		for(int j = 0; j <= order -1; j++){
-			y[order-1] += stepsize*(output_a[j])*(y[order-1-j]);
-		}
-		y[order-1] -= stepsize*(coefficients[order+1]/coefficients[0]);
-		//(yn-1)(t+h) = (yn-1)(t)+h*((-(an-1)/an)*(yn-1)+(-(an-2)/an)*(yn-2)+...+(-a0/an)*(y)+(-c/an))
-		for(int n = order - 2; n>=0; n--){
-			y[n] = y[n]+stepsize*y[n+1];
-		}
-		//rest yi(t+h) = yi(t) + h*(yi+1)(t)
-		yvalues[i] = y[0];
+		vector_y = Matmul(matrix,vector_y,order+1,order+1,1);
+		yvalues[i] = vector_y[1][0];
 		//recorded y(t) in each iteration in yvalues which should be the output
 	}
 	return yvalues;
@@ -50,7 +51,7 @@ int main(){
     double lowerbound = 0.0;
     double upperbound = 10.0;
     //taking the datapoints between 0 and 10
-    double stepsize = 0.01;// h = 0.01
+    double stepsize = 0.1;// h = 0.01
 
     // Record data using the provided recorddata function
     double* results = recorddata(lowerbound, upperbound, order, coefficients, initialconditions, stepsize);
